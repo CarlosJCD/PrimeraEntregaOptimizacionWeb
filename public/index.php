fde9becd3d793035533becf7d5bbbcb54ad15631
CarlosJCD <carlos.calderon.d@icloud.com>
2024-02-15 18:17:22 -0600

    Added app routes

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\CtrlFeeds;
+use Controllers\CtrlNews;
+use Controllers\CtrlPages;
 use MVC\Router;
 
 $router = new Router();
@@ -9,6 +12,14 @@ $router = new Router();
 
 // $router->get('/', [PaginasController::class, 'index']);
 
+$router->get("/",[CtrlPages::class,"index"]);
+$router->get("/feeds",[CtrlPages::class,"feeds"]);
 
+$router->put("/news/update",[CtrlNews::class,"updateNews"]);
+
+
+$router->post("/feeds/new",[CtrlFeeds::class,""]);
+$router->put("/feeds/update/",[CtrlFeeds::class,"updateFeed"]);
+$router->delete("/feeds/delete/",[CtrlFeeds::class,"updateFeed"]);
 
 $router->comprobarRutas();
